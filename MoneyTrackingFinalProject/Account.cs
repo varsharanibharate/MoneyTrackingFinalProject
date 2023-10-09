31ab1cce47b8fd668a9e263bb98ce3ee81a9d083
Varsharani Bharate <varsharanibharate@gmail.com>
2023-10-09 15:55:35 +0200

    Added UML Class diagram

---
+++ MoneyTrackingFinalProject/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoneyTrackingFinalProject
+{
+    public class Account
+    {
+        public int Id;
+        public string Type;
+        public string Title;
+        public int Amount;
+        public DateTime Date;
+    }
+
+    public class Income : Account
+    {
+        public void Constructor()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Expenses : Account
+    {
+        public void Constructor()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ProcessTransactionDetails
+    {
+        public Account Account
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public list readDataFromFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void writeDataToFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void editList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public list printAccountList()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
